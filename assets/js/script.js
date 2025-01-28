dbdd6aeb2b54515bd4bac151e149cdaa81fe4ca5
Tomislav Dukez <tomdu3@ymail.com>
2025-01-28 16:02:38 +0000

    add flip card css code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,1 @@
-const h1 = document.createElement('h1');
-h1.classList.add('title');
-h1.textContent = 'This is the best possible header';
-h1.id = 'mainHeader';
-document.body.appendChild(h1);
+console.log('Loading function');
